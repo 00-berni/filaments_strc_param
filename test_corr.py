de1e6914dddedd8b22a7ee4fc1915238df86f776
00-berni <bernardo.vettori@outlook.it>
2025-08-06 19:05:35 +0200

    Work on logging

--- test_corr.py
+++ test_corr.py
@@ -3,108 +3,50 @@ import matplotlib.pyplot as plt
 import filpy
 from scipy.fft import fft,ifft, fft2, ifft2
 from filpy import distance
-from scipy.signal import correlate2d, correlation_lags
-from scipy.spatial import KDTree
-
 from time import time
+import logging
+logging.getLogger(__name__)
 
-dim = 22
+## DATA
+FILE_NAME = filpy.FileVar(__file__,path=True)   #: path of the file
+logger = logging.basicConfig(filename=filpy.log_path(FILE_NAME),filemode='w', encoding='utf-8',level=logging.ERROR)
+dim = 31                                        #: size of the field 
+# build the field
 xx, yy = np.meshgrid(np.arange(dim),np.arange(dim))
 np.random.seed(10)
 data = np.random.random((dim,dim)) 
-PARAM = 3
+PARAM = 5
 data = np.zeros((dim,dim))
 data[::PARAM,::PARAM] = 1
-
-data_mean = data.mean()
-
-def mydist(*args):
-    return distance(*args)
-
 filpy.show_image(data,cmap='viridis')
-# plt.show()
-# exit()
-all_pos = [ (i,j) for i in range(data.shape[0]) for j in range(data.shape[1])]
-all_pos = np.asarray(all_pos).T
-# start = time()
-# distances = [distance(all_pos[:,N],all_pos[:,N:]) for N in range(all_pos.shape[1])]
-# end = time()
-# print('Parallel: ', end-start, 's')
-# del distances
-# start = time()
-# distances = [] 
-# for N in range(all_pos.shape[1]):
-#     distances += [mydist(all_pos[:,N],all_pos[:,N:])]
-# end = time()
-# print('Sequential: ', end-start, 's')
+
+# log = filpy.Log_File(FILE_NAME,print_cond=True)
 
 
+## PIPELINE
+data_mean = data.mean()
+field = np.copy(data) - data_mean
 
 
-# exit()
-def corr_r(lag:float):
-    for N in range(len(all_pos)):
-        val0 = data[tuple(all_pos[:,N])]
-# dist = np.linspace(0,150,300)
-# signal = KDTree(data)
-# random = KDTree(np.random.random((dim,dim)))
-# DD = signal.count_neighbors(signal,dist)
-# DR = signal.count_neighbors(random,dist)
-# RR = random.count_neighbors(random,dist)
-# # f = len(signal)/len(random)
-# corr = (DD - 2*DR  + RR)/RR
-# print(corr)
-# filpy.quickplot((dist,corr),fmt='.--')
-# plt.show()
+all_pos = np.array([ (i,j) for i in range(field.shape[0]) for j in range(field.shape[1])]).T
 
-
-# corr = ifft2(fft2(data)**2)
-# filpy.show_image(np.real(corr),cmap='viridis')
-# filpy.show_image(np.abs(corr),show=True,cmap='viridis')
-# filpy.quickplot(np.real(corr).diagonal(),fmt='.--')
-# filpy.quickplot(np.abs(corr).diagonal(),fmt='.--')
-# plt.show()
-
-# @filpy.timeit
-def calc_corr_ij(pos: np.ndarray, positions: np.ndarray) -> tuple[np.ndarray,np.ndarray]:
-    x,y = pos
-    val0 = data[x,y] - data_mean
-    # dists = distance(pos,positions)
-    print(positions.shape,end='\r')
-    corr_ij = val0*(data[*positions]-data_mean)
-    return corr_ij
-
-all_pos = [ (i,j) for i in range(data.shape[0]) for j in range(data.shape[1])]
-all_pos = np.asarray(all_pos).T
-
+logger.info('Compute all the distances')
 start = time()
 res_dist = np.concatenate([distance(all_pos[:,N],all_pos[:,N:]) for N in range(all_pos.shape[1])])
-# res_dist = np.array([list(distance(all_pos[:,N],all_pos[:,N:])) for N in range(all_pos.shape[1])],dtype='object').sum()
 end = time()
-print('Compilation time:', end-start,'s')
-print(type(res_dist),len(res_dist))
-# del res_dist
-# start = time()
-# res_corr = [calc_corr_ij(all_pos[:,N],all_pos[:,N:]) for N in range(all_pos.shape[1])]
-# end = time()
+logger.info(f'Compilation time: {end-start} s')
+logger.info('Compute all the distances')
 start1 = time()
-res_corr = np.concatenate([calc_corr_ij(all_pos[:,N],all_pos[:,N:]) for N in range(all_pos.shape[1])])
+res_corr = np.concatenate([field[*all_pos[:,N]] * field[*all_pos[:,N:]] for N in range(all_pos.shape[1])])
 end1 = time()
-# print('Compilation time:', end-start,'s')
-print('Compilation time:', end1-start1,'s')
+logger.info(f'Compilation time: {end1-start1} s')
 
-
-# exit()
-# distances = np.ravel([r[0] for r in res])
-# correlations = np.ravel([r[1] for r in res])
-print('dist')
+logger.info('Compute the correlation')
+start1 = time()
 unq_dist = np.unique(res_dist)
-print('dist end')
 correlations = np.array([np.sum(res_corr[res_dist == d]) for d in unq_dist])
-print('Compilation time:', end1-start1,'s')
-# correlations = abs(correlations)
-
-# correlations /= correlations.max()
+end1 = time()
+logger.info(f'Compilation time: {end1-start1} s')
 
 filpy.quickplot((unq_dist,correlations),fmt='.--')
 for i in range(10):
@@ -114,35 +56,36 @@ for i in range(10):
             break
         plt.axvline(d,color='red',linestyle='dotted')
 plt.show()
+
 
 ## FFT
-data_fft = fft2(data)
-tmp = data_fft * np.conjugate(data_fft)
-tmp_img = np.abs(ifft2(np.abs(tmp)))
-del tmp
-MID = dim // 2
-print(tmp_img.shape,MID)
-corr_img = np.copy(tmp_img)
-corr_img[ : MID, : MID] = tmp_img[MID : , MID : ]
-corr_img[MID : , MID : ] = tmp_img[ : MID, : MID]
-corr_img[ : MID, MID : ] = tmp_img[MID : , : MID]
-corr_img[MID : , : MID] = tmp_img[ : MID, MID : ]
-filpy.show_image(tmp_img)
-filpy.show_image(corr_img)
+# data_fft = fft2(data)
+# tmp = data_fft * np.conjugate(data_fft)
+# tmp_img = np.abs(ifft2(np.abs(tmp)))
+# del tmp
+# MID = dim // 2
+# print(tmp_img.shape,MID)
+# corr_img = np.copy(tmp_img)
+# corr_img[ : MID, : MID] = tmp_img[MID : , MID : ]
+# corr_img[MID : , MID : ] = tmp_img[ : MID, : MID]
+# corr_img[ : MID, MID : ] = tmp_img[MID : , : MID]
+# corr_img[MID : , : MID] = tmp_img[ : MID, MID : ]
+# filpy.show_image(tmp_img)
+# filpy.show_image(corr_img)
 
-distances = np.array([ [distance((i,j),(MID+0.5,MID+0.5)) for i in range(dim)] for j in range(dim)])
-unq_dist = np.unique(distances)
+# distances = np.array([ [distance((i,j),(MID+0.5,MID+0.5)) for i in range(dim)] for j in range(dim)])
+# unq_dist = np.unique(distances)
 
-correlations = np.array([np.sum(corr_img[distances == d]) for d in unq_dist])
+# correlations = np.array([np.sum(corr_img[distances == d]) for d in unq_dist])
 
-filpy.quickplot((unq_dist,correlations),fmt='.--')
-for i in range(10):
-    for j in range(10):
-        d = distance((0,0),(i,j))*PARAM
-        if d > np.max(unq_dist):
-            break
-        plt.axvline(d,color='red',linestyle='dotted')
-plt.show()
+# filpy.quickplot((unq_dist,correlations),fmt='.--')
+# for i in range(10):
+#     for j in range(10):
+#         d = distance((0,0),(i,j))*PARAM
+#         if d > np.max(unq_dist):
+#             break
+#         plt.axvline(d,color='red',linestyle='dotted')
+# plt.show()
 
 # # #
 
