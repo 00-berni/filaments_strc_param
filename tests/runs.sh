ef1f601c1c971c0f4b6bddce5620ed40476afbe7
00-berni <bernardo.vettori@outlook.it>
2025-09-30 15:16:48 +0200

    Update

--- tests/runs.sh
+++ tests/runs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for i in 100 500 700 1000
 do
+    echo "START PROCESS DIM : $i"
     python3 filaments_strc_param/tests/test_mapping.py >> ./filaments_strc_param/tests/test_mapping.log --dim $i --lag 10 --method sequence
     python3 filaments_strc_param/tests/test_mapping.py >> ./filaments_strc_param/tests/test_mapping.log --dim $i --lag 10 --method parallel
+    echo "END PROCESS"
+    echo ""
 done
