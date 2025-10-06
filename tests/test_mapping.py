9053c4d6ae707741a4835e14180622cedf55134e
00-berni <bernardo.vettori@outlook.it>
2025-10-06 18:43:30 +0200

    Treatment of Boundary conditions

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -6,94 +6,92 @@ from multiprocessing import Pool, cpu_count
 from functools import partial
 from numpy.typing import ArrayLike
 
-# from .test_func import *
+from .test_func import *
 # from .test_func import distance
 
 
 
-def __mapping(coord: tuple[int,int], positions: tuple[np.ndarray,np.ndarray]) -> tuple[tuple[np.ndarray,np.ndarray],tuple[np.ndarray,np.ndarray]]:
-    i,j = coord
-    # ydim, xdim = data.shape
-    # xx, yy = np.meshgrid(np.arange(xdim),np.arange(ydim))
-    xx, yy = positions
-    if i == 0:
-        xpos = slice(None,None) 
-    elif i > 0:
-        xpos = slice(None,-i)
-    else:
-        xpos = slice(-i,None)
-
-    if j == 0:
-        ypos = slice(None,None) 
-    elif j > 0:
-        ypos = slice(None,-j)
-    else:
-        ypos = slice(-j,None)
-
-    x = xx[ypos,xpos]
-    y = yy[ypos,xpos]
-    x_pos = (x,x+i)
-    y_pos = (y,y+j)
-    return x_pos, y_pos     
 
 
-def compute(data: np.ndarray):
+def compute(data: np.ndarray, mask_ends: tuple[tuple[int,int], tuple[int,int]]):
     tmp_data = data - np.mean(data)
     ydim, xdim = tmp_data.shape
-    xx, yy = np.meshgrid(np.arange(xdim),np.arange(ydim))
-    corr = np.zeros((*xx.shape,4))
-    xsgn = (0,0,-1,-1)
-    ysgn = (0,-1,-1,0)
-    for m in range(xx.size):
-        ii,jj = np.unravel_index(m,xx.shape)
-        i = xx[ii,jj]
-        j = yy[ii,jj]
-        x_pos, y_pos = __mapping((i,j),(xx,yy))
-        corr[ii,jj] = [np.sum(tmp_data[y_pos[k],x_pos[t]]*tmp_data[y_pos[k+1],x_pos[t+1]]) for k,t in zip(ysgn,xsgn)]
+    (xo, xe), (yo, ye) = mask_ends
+    m_ydim = ye-yo 
+    m_xdim = xe-xo
+    max_lag = min(xo,yo,xdim-xe,ydim-ye,int(np.sqrt(m_xdim**2+m_ydim**2)))
+    corr_dim = max_lag+1
+    xx, yy = np.meshgrid(np.arange(m_xdim),np.arange(m_ydim))
+    xx += xo
+    yy += yo
+    corr = np.zeros((corr_dim,corr_dim,4))
+    xsgn = (1,1,-1,-1)
+    ysgn = (1,-1,-1,1)
+    for j in range(corr_dim):
+        for i in range(corr_dim):
+            corr[j,i] = [np.sum(tmp_data[yy,xx] * tmp_data[yy+t*j,xx+k*i]) if i**2+j**2 <= max_lag**2 else 0 for k,t in zip(xsgn,ysgn) ]
     return np.asarray(corr)
 
-def step0(inputs: list[ArrayLike]):
-    step = inputs[0]
-    coord = inputs[1]
-    data = inputs[2]
-
-    xsgn = (0,0,-1,-1)
-    ysgn = (0,-1,-1,0)
-    xx,yy = coord
-    ii,jj = np.unravel_index(step,xx.shape)
-    i = xx[ii,jj]
-    j = yy[ii,jj]
-    x_pos, y_pos = __mapping((i,j),(xx,yy))
-    corr_i = [np.sum(data[y_pos[k],x_pos[t]]*data[y_pos[k+1],x_pos[t+1]]) for k,t in zip(ysgn,xsgn)]
-    return corr_i
-
 def step1(step: int):
-    xsgn = (0,0,-1,-1)
-    ysgn = (0,-1,-1,0)
-    ii,jj = np.unravel_index(step,g_xx.shape)
-    i = g_xx[ii,jj]
-    j = g_yy[ii,jj]
-    x_pos, y_pos = __mapping((i,j),(g_xx,g_yy))
-    corr_i = [np.sum(g_tmp_data[y_pos[k],x_pos[t]]*g_tmp_data[y_pos[k+1],x_pos[t+1]]) for k,t in zip(ysgn,xsgn)]
-    return corr_i
+    xsgn = (1,1,-1,-1)
+    ysgn = (1,-1,-1,1)
+    j,i = step 
+    if i**2+j**2 <= g_max_lag**2:
+        return [np.sum(g_tmp_data[g_yy,g_xx] * g_tmp_data[g_yy+t*j,g_xx+k*i]) for k,t in zip(xsgn,ysgn)]
+    else:
+        return [0,0,0,0]
 
 
-def parallel_compute(data: np.ndarray, processes: int = cpu_count()-1):
-    global g_tmp_data
+def parallel_compute(data: np.ndarray, mask_ends: tuple[tuple[int,int], tuple[int,int]], processes: int = cpu_count()-1):
+    global g_tmp_data, g_max_lag
     g_tmp_data = np.copy(data) - np.mean(data)
     ydim, xdim = data.shape
+    (xo, xe), (yo, ye) = mask_ends
+    m_ydim = ye-yo 
+    m_xdim = xe-xo
+    g_max_lag = min(xo,yo,xdim-xe,ydim-ye,int(np.sqrt(m_xdim**2+m_ydim**2)))
+    corr_dim = g_max_lag+1
     global g_xx, g_yy
-    g_xx, g_yy = np.meshgrid(np.arange(xdim),np.arange(ydim))
-    # print('DATA',data[3])
-    # print('PARAL_DATA',tmp_data[3])
-    # iterator = [[i, (g_xx,g_yy), tmp_data, corr ] for i in np.arange(xx.size)]
+    g_xx, g_yy = np.meshgrid(np.arange(m_xdim),np.arange(m_ydim))
+    g_xx += xo
+    g_yy += yo
+    positions = np.asarray(np.meshgrid(np.arange(corr_dim),np.arange(corr_dim))).T.reshape(corr_dim*corr_dim,2)
     with Pool(processes=processes) as pool:
-        # corr_i = pool.map(step0,iterator)
-        corr_i = pool.map(step1, np.arange(g_xx.size))
-    return np.asarray(corr_i).reshape(ydim,xdim,4)
+        corr_i = pool.map(step1, positions)
+    return np.asarray(corr_i).reshape(corr_dim,corr_dim,4)
 
 
 if __name__ == '__main__':
+    def make_pattern(data: np.ndarray, pattern: str = 'lattice',**kwargs) -> np.ndarray:
+        new_data = np.copy(data)
+        xdim, ydim = new_data.shape
+        if pattern == 'lattice':
+            wd = kwargs['width']
+            sp = kwargs['spacing']
+            lag = sp+wd
+            xpos = np.arange(xdim//lag+1)*lag
+            ypos = np.arange(ydim//lag+1)*lag
+            xpos = np.concatenate([xpos + i for i in range(wd)])
+            ypos = np.concatenate([ypos + i for i in range(wd)])
+            xpos = xpos[xpos<xdim]
+            # ypos = ypos[ypos<ydim]
+            new_data[xpos,:] += 1
+            # new_data[:,ypos] = 1
+        elif pattern == 'filament':
+            wd = kwargs['width']
+            sp = kwargs['spacing']
+            sequence = [0,1,2,3,3,3,2,2,1,1,0,-1,-2,-1,-1,-2,-3,-3,-4,-4,-3,-2,-2,-1,-1]
+            patt = np.array(sequence*(ydim//len(sequence))+sequence[:ydim%len(sequence)])
+            pos  = np.arange(ydim)
+            
+            for i in range(wd):
+                xpos = sp+patt + i
+                idx = xpos<xdim
+                xpos = xpos[idx]
+                pos = pos[idx]
+                new_data[xpos,pos] += 1
+        return new_data
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--log",help='set log',nargs='*',type=str,action="store",choices=['file','bash','all','DEBUG', 'INFO'],default=None)
     parser.add_argument("--no-diag", help='compute horizontal and vertical only', action='store_false')
@@ -104,6 +102,7 @@ if __name__ == '__main__':
     parser.add_argument("-e","--edges", help='set max and min of noise', type=float, required=False, nargs=2, action='store',default=[0,2])
     parser.add_argument("-w","--width", help='set the width of the pattern', required=False, type=int, action='store',default=3)
     parser.add_argument("-l","--lag", help='set the lag of the lattice', required=False, type=int, action='store',default=10)
+    parser.add_argument("--maxlag", help='set the max lag', type=int, action='store',default=10)
     parser.add_argument("-v","--value", help='set the value of the lattice', required=False, type=int, action='store',default=1)
     parser.add_argument("-i","--iter", help='set the number of iterations',required=False, type=int, action='store',default=10)
     parser.add_argument("-p","--plot",help='plot data or not',action='store_false')
@@ -115,46 +114,55 @@ if __name__ == '__main__':
     dim = args.dim
     data = np.zeros((dim,dim)) + np.random.normal(0.5,0.3,size=(dim,dim))
     ydim, xdim = data.shape
-    lag = args.lag
-    data[::lag] = 1
-    data[1::lag] = 1
-    data[2::lag] = 1
+    max_lag = args.maxlag
+    xlim = (max_lag, xdim-max_lag)
+    ylim = (max_lag, ydim-max_lag)
+    data = make_pattern(data,pattern='filament',width=args.width,spacing=args.maxlag+args.lag)
     print('FRAME: ',(dim,dim))
     print('METHOD: ',args.method.upper())
 
 
     if args.method == 'sequence':    
         start_time = time()
-        corr = compute(data)
+        corr = compute(data,mask_ends=(xlim,ylim))
         print(corr.shape)
-        # print('CORR\n',corr[:,0,:])
-        tot_corr = np.zeros((2*ydim-1,2*xdim-1))
-        tot_corr[ydim-1:,xdim-1:] = corr[:,:,0]
-        tot_corr[:ydim-1,xdim:] = corr[::-1,:,1][:-1,1:]
-        tot_corr[:ydim,:xdim] = corr[::-1,::-1,2]
-        tot_corr[ydim:,:xdim-1] = corr[:,::-1,3][1:,:-1]
+        corr_dim = corr.shape[0] 
+        tot_corr = np.zeros((2*corr_dim-1,2*corr_dim-1))
+        tot_corr[corr_dim-1:,corr_dim-1:] = corr[:,:,0]
+        tot_corr[:corr_dim-1,corr_dim:] = corr[::-1,:,1][:-1,1:]
+        tot_corr[:corr_dim,:corr_dim] = corr[::-1,::-1,2]
+        tot_corr[corr_dim:,:corr_dim-1] = corr[:,::-1,3][1:,:-1]
         end_time = time()
         print('\nCOMPUTATIONAL TIME: ',(end_time-start_time)/60,' m')
-        print(tot_corr[ydim-1,xdim-1],np.max(tot_corr))
+        print(tot_corr[corr_dim-1,corr_dim-1],np.max(tot_corr))
         # print(data[0])
 
     ## PARALLEL
     elif args.method == 'parallel':    
         start_time = time()
-        corr = parallel_compute(data,processes=args.cores)
+        corr = parallel_compute(data,mask_ends=(xlim,ylim),processes=args.cores)
         print('After paral:', corr.shape)
-        tot_corr = np.zeros((2*ydim-1,2*xdim-1))
-        tot_corr[ydim-1:,xdim-1:] = corr[:,:,0]
-        tot_corr[:ydim-1,xdim:] = corr[::-1,:,1][:-1,1:]
-        tot_corr[:ydim,:xdim] = corr[::-1,::-1,2]
-        tot_corr[ydim:,:xdim-1] = corr[:,::-1,3][1:,:-1]
+        corr_dim = corr.shape[0] 
+        # print('CORR\n',corr[:,0,:])
+        tot_corr = np.zeros((2*corr_dim-1,2*corr_dim-1))
+        tot_corr[corr_dim-1:,corr_dim-1:] = corr[:,:,0]
+        tot_corr[:corr_dim-1,corr_dim:] = corr[::-1,:,1][:-1,1:]
+        tot_corr[:corr_dim,:corr_dim] = corr[::-1,::-1,2]
+        tot_corr[corr_dim:,:corr_dim-1] = corr[:,::-1,3][1:,:-1]
         end_time = time()
         print('\nCOMPUTATIONAL TIME: ',(end_time-start_time)/60,' m')
-        print(tot_corr[ydim-1,xdim-1],np.max(tot_corr))
+        print(tot_corr[corr_dim-1,corr_dim-1],np.max(tot_corr))
     print('\n'+'======'*10+'\n') 
     
-    # plt.figure()
-    # plt.imshow(data,origin='lower')
-    # plt.figure()
-    # plt.imshow(tot_corr,origin='lower')
-    # plt.show()
+    plt.figure()
+    plt.imshow(data,origin='lower')
+    plt.plot([max_lag,max_lag,xdim-max_lag,xdim-max_lag,max_lag],[max_lag,ydim-max_lag,ydim-max_lag,max_lag,max_lag],color='red')
+    plt.figure()
+    tot_corr[corr_dim-1,corr_dim-1] = 0
+    plt.imshow(tot_corr,origin='lower')
+
+    plt.figure()
+    tot_corr = filpy.asym_tpcf(data,(xlim,ylim),result='cum',zero_cover=True)
+    plt.imshow(tot_corr,origin='lower')
+
+    plt.show()
