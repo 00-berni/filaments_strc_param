7dee4911f7b89b9ea1bb1c3204439ce20ebebee5
00-berni <bernardo.vettori@outlook.it>
2025-10-07 11:54:08 +0200

    Update for the next run

--- tests/runs.sh
+++ tests/runs.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-for i in 100 500 700 1000
+dimensions=(100 500 700 1000 2000)
+maxlags=(10 100 200 400 500)
+indexes=(0 1 2 3 4)
+
+for i in ${indexes[0]}
 do
-    echo "START PROCESS DIM : $i"
-    python3 filaments_strc_param/tests/test_mapping.py >> ./filaments_strc_param/tests/test_mapping.log --dim $i --lag 10 --method sequence
-    python3 filaments_strc_param/tests/test_mapping.py >> ./filaments_strc_param/tests/test_mapping.log --dim $i --lag 10 --method parallel
+    echo "===================================="
+    echo "START PROCESS DIM ${dimensions[$i]}"
+    current_time="`date "+%H:%M:%S"`"
+    echo "Start Time: $current_time"
+    echo "> start sequencial"
+    python3 -m filaments_strc_param.tests.test_speed >> ./filaments_strc_param/tests/test_speed.log --dim ${dimensions[$i]} --maxlag ${maxlags[$i]} --method sequence
+    echo "> end sequencial"
+    echo "> start parallel"
+    python3 -m filaments_strc_param.tests.test_speed >> ./filaments_strc_param/tests/test_speed.log --dim ${dimensions[$i]} --maxlag ${maxlags[$i]} --method parallel
+    echo "> end parallel"
+    current_time="`date "+%H:%M:%S"`"
+    echo "End Time: $current_time"
     echo "END PROCESS"
+    echo "===================================="
     echo ""
 done
