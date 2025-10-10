cd2dacad1492033bb50a45a86a1baa33af989d2b
00-berni <bernardo.vettori@outlook.it>
2025-10-10 18:25:37 +0200

    Remove usless stuff

--- filpy/__init__.py
+++ filpy/__init__.py
@@ -4,8 +4,6 @@ from .stuff import *
 from .fit_stuff import get_data_fit
 import logging
 
-filpy_logger = logging.getLogger(__name__)
-filpy_logger.info('Prova')
-INITIAL_STR = f"Package Dir: {PKG_DIR}\nProject Dir: {PROJECT_DIR}"
+# INITIAL_STR = f"Package Dir: {PKG_DIR}\nProject Dir: {PROJECT_DIR}"
 
-print(INITIAL_STR)
+# print(INITIAL_STR)
