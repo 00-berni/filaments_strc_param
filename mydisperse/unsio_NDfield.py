26a539974fb0f841ec50613204725622b513dbad
00-berni <bernardo.vettori@outlook.it>
2025-11-19 12:57:21 +0100

    Add documentation

--- mydisperse/unsio_NDfield.py
+++ mydisperse/unsio_NDfield.py
@@ -13,32 +13,49 @@ from __future__ import print_function
 import unsio.input as uns_in
 import numpy as np
 from .NDfield import write_NDfield_coord
+from argparse import Namespace
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # process function
-def process(args):
+def process(args: Namespace) -> None:
+    """_summary_
+
+    Parameters
+    ----------
+    args : Namespace
+        arguments of parse in `commandLine()`. Arguments are:
+            * `input`      : str  = input snapshot
+            * `output`     : str  = out NDfiled file
+            * `components` : list = list of components eg:gas
+            * `verbose`    : bool = parameter
+    """
     uns=uns_in.CUNS_IN(args.input,args.components,"all")
 
-    if uns.nextFrame("") : # load snapshot
-        ok,tsnap=uns.getData("time") # return snasphot time
-        ok,pos=uns.getData(args.components,"pos") # read positions
+    # load snapshot
+    if uns.nextFrame("") : 
+        # return snasphot time
+        ok,tsnap=uns.getData("time") 
+        # read positions
+        ok,pos=uns.getData(args.components,"pos") 
         if ok:
-            pos=np.reshape(pos,(-1,3)).T # reshape pos to 2D array (3,nbody)
+            # reshape pos to 2D array (3,nbody)
+            pos=np.reshape(pos,(-1,3)).T 
             write_NDfield_coord(pos,args.output)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # commandLine, parse the command line
-def commandLine():
+def commandLine() -> None:
+    """Interactive command to save in .NDfield file"""
     import argparse
-     # help
+    # help
     parser = argparse.ArgumentParser(description="Build NDfield file for DISPERSE analysis",
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # options
-    parser.add_argument('input', help='input snaphot (unsio compliant)', type=str)
-    parser.add_argument('output', help='out NDfiled file', type=str)
+    parser.add_argument('input',      help='input snapshot (unsio compliant)', type=str)
+    parser.add_argument('output',     help='out NDfiled file', type=str)
     parser.add_argument('components', help='list of components eg:gas')
-    parser.add_argument('--verbose',help='verbose mode',dest="verbose", action="store_true", default=False)
-     # parse
+    parser.add_argument('--verbose',  help='verbose mode',dest="verbose", action="store_true", default=False)
+    # parse
     args = parser.parse_args()
 
     # start main funciton
