2ce42e82b776dffb7471eefe8e959d2b407b052a
00-berni <bernardo.vettori@outlook.it>
2025-11-24 15:36:41 +0100

    Collect types

---
+++ filpy/typing.py
@@ -0,0 +1,20 @@
+from typing import Literal, Sequence
+from numpy.typing import NDArray, ArrayLike
+from numpy import int_, float64, bool_
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from astropy.io.fits import HDUList
+
+IntArray   = NDArray[int_]
+FloatArray = NDArray[float64]
+BoolArray  = NDArray[bool_]
+
+__all__ = [
+           'Literal',
+           'Sequence',
+           'NDArray',
+           'ArrayLike',
+           'IntArray',
+           'FloatArray',
+           'BoolArray'
+          ]
