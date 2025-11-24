15d0c56e0bceef06024ba39c89ba822a8b38b565
00-berni <bernardo.vettori@outlook.it>
2025-11-24 15:07:55 +0100

    Update types

--- mydisperse/types.py
+++ mydisperse/types.py
@@ -1,4 +1,9 @@
 from typing import Literal
+from numpy.typing import NDArray
+from numpy import int_, float64, bool_
 
+IntArray   = NDArray[int_]
+FloatArray = NDArray[float64]
+BoolArray  = NDArray[bool_]
 
-__all__ = ['Literal']
+__all__ = ['Literal','IntArray','FloatArray','BoolArray']
