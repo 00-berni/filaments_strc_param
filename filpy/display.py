3a7eb650ce4c5a917fefbf4ce7d93a6500ce2c50
00-berni <bernardo.vettori@outlook.it>
2026-01-05 21:40:03 +0100

    Add methods to plot

--- filpy/display.py
+++ filpy/display.py
@@ -4,12 +4,125 @@ from .typing import *
 from .typing import Axes, Figure
 
 __all__ = [
+            'myplot',
             'quickplot',
+            'v_lines',
+            'h_lines',
             'plot_image',
             'show_image'
           ]
 
-def quickplot(data: Union[Sequence[NDArray], NDArray], numfig: int = None, fmt: str = '-', title: str = '', labels: Sequence[str] = ('',''), dim: Sequence[int] = [10,7], grid: bool = False,**pltargs) -> None:
+def v_lines(ax: Axes, lines: Union[float, list[float]], colors: Union[Optional[str], list[Optional[str]]] = None, linestyles: Union[Optional[str], list[Optional[str]]] = None, labels: Union[Optional[str],list[Optional[str]]] = None,**pltkwargs) -> None:
+    """Plot vertical license
+
+    Parameters
+    ----------
+    ax : Axes
+        subplot
+    lines : Union[float, list[float]]
+        line value or a list of lines
+    colors : Union[Optional[str], list[Optional[str]]], optional
+        color/s of the value/s, by default None
+    linestyles : Union[Optional[str], list[Optional[str]]], optional
+        line style/s of the value/s, by default None
+    labels : Union[Optional[str],list[Optional[str]]], optional
+        label/s of the value/s, by default None
+    """
+    if not isinstance(lines,list):
+        lines = [lines]
+    if not isinstance(colors,list):
+        colors = [colors]*len(lines)
+    if not isinstance(linestyles,list):
+        linestyles = [linestyles]*len(lines)
+    if not isinstance(labels,list):
+        labels = [labels]*len(lines)
+
+    if len(colors) < len(lines):
+        colors += [colors[-1]]*(len(lines)-len(colors))
+    if len(linestyles) < len(lines):
+        linestyles += [linestyles[-1]]*(len(lines)-len(linestyles))
+    if len(labels) < len(lines):
+        labels += [labels[-1]]*(len(lines)-len(labels))
+
+    for line, color, linestyle, label in zip(lines,colors,linestyles,labels):
+        ax.axvline(line,color=color,linestyle=linestyle,label=label,**pltkwargs)
+
+def h_lines(ax: Axes, lines: Union[float, list[float]], colors: Union[Optional[str], list[Optional[str]]] = None, linestyles: Union[Optional[str], list[Optional[str]]] = None, labels: Union[Optional[str],list[Optional[str]]] = None,**pltkwargs) -> None:
+    """Plot horizontal license
+
+    Parameters
+    ----------
+    ax : Axes
+        subplot
+    lines : Union[float, list[float]]
+        line value or a list of lines
+    colors : Union[Optional[str], list[Optional[str]]], optional
+        color/s of the value/s, by default None
+    linestyles : Union[Optional[str], list[Optional[str]]], optional
+        line style/s of the value/s, by default None
+    labels : Union[Optional[str],list[Optional[str]]], optional
+        label/s of the value/s, by default None
+    """
+    if not isinstance(lines,list):
+        lines = [lines]
+    if not isinstance(colors,list):
+        colors = [colors]*len(lines)
+    if not isinstance(linestyles,list):
+        linestyles = [linestyles]*len(lines)
+    if not isinstance(labels,list):
+        labels = [labels]*len(lines)
+
+    if len(colors) < len(lines):
+        colors += [colors[-1]]*(len(lines)-len(colors))
+    if len(linestyles) < len(lines):
+        linestyles += [linestyles[-1]]*(len(lines)-len(linestyles))
+    if len(labels) < len(lines):
+        labels += [labels[-1]]*(len(lines)-len(labels))
+
+    for line, color, linestyle, label in zip(lines,colors,linestyles,labels):
+        ax.axhline(line,color=color,linestyle=linestyle,label=label,**pltkwargs)
+
+def myplot(fig: Figure, ax: Axes, data: Union[Sequence[NDArray], NDArray], title: str = '', suptitle: str = '',xlabel: str = '', ylabel: str = '',grid: bool = False, fontsize: int = 18,**pltkwargs) -> None:
+    """Plot data
+
+    Parameters
+    ----------
+    fig : Figure
+        figure
+    ax : Axes
+        subplot
+    data : Union[Sequence[NDArray], NDArray]
+        data to plot
+    title : str, optional
+        the title, by default ''
+    suptitle : str, optional
+        the suptitle, by default ''
+    xlabel : str, optional
+        xlabel, by default ''
+    ylabel : str, optional
+        ylabel, by default ''
+    grid : bool, optional
+        _description_, by default False
+    fontsize : int, optional
+        _description_, by default 18
+    """
+    title_font = fontsize + 2 
+    fig.suptitle(suptitle,fontsize=title_font)
+    ax.set_title(title,fontsize=title_font)
+    if not isinstance(data,Sequence):
+        data = [data]
+    if (len(data) <= 2) and ('yerr' not in pltkwargs) and ('xerr' not in pltkwargs):
+        ax.plot(*data,**pltkwargs)
+    else:
+        ax.errorbar(*data,**pltkwargs)
+    ax.set_xlabel(xlabel,fontsize=fontsize)
+    ax.set_ylabel(ylabel,fontsize=fontsize)
+    if grid: 
+        ax.grid(which='both',linestyle='--',alpha=0.2,color='grey')
+
+
+
+def quickplot(data: Union[Sequence[NDArray], NDArray], numfig: Optional[int] = None, figsize: tuple[int,int] = (10,7), output: bool = False, title: str = '', suptitle: str = '',xlabel: str = '', ylabel: str = '',grid: bool = True, fontsize: int = 18,**pltkwargs) -> Optional[tuple[Figure, Axes]]:
     """Display a plot quickly.
 
     Parameters
@@ -39,19 +152,19 @@ def quickplot(data: Union[Sequence[NDArray], NDArray], numfig: int = None, fmt: 
     (I wrote it only because of my laziness in writing code).
 
     """
-    if 'fontsize' not in pltargs.keys():
-        pltargs['fontsize'] = 18
-    font_size = pltargs['fontsize']
-    pltargs.pop('fontsize')
-    xl,yl = labels
-    plt.figure(numfig,figsize=dim)
-    plt.title(title,fontsize=font_size+2)
-    if isinstance(data, np.ndarray): data = [np.arange(len(data)),data]
-    plt.errorbar(*data,fmt=fmt,**pltargs)
-    plt.xlabel(xl,fontsize=font_size)
-    plt.ylabel(yl,fontsize=font_size)
-    if grid: plt.grid(which='both',linestyle='--',alpha=0.2,color='grey')
-
+    fig = plt.figure(num=numfig,figsize=figsize)
+    ax = fig.add_subplot()
+    myplot(fig=fig,ax=ax,
+           data=data,
+           title=title,
+           suptitle=suptitle,
+           xlabel=xlabel,
+           ylabel=ylabel,
+           grid=grid,
+           fontsize=fontsize,
+           **pltkwargs)
+    if output:
+        return fig, ax
 
 def plot_image(fig: Figure, ax: Axes, data: NDArray, subtitle: str = '',colorbar: bool = True, **figargs) -> None:
     """To plot fits images
